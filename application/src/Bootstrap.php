20b2c9ccfa14a8c978c5d25aeedc63575b087cc0
Jason <jasonrobertfox@gmail.com>
2012-10-13 23:42:53 -0400

    added a basic ui layer and some default handlers

--- application/src/Bootstrap.php
+++ application/src/Bootstrap.php
@@ -14,11 +14,53 @@ class Bootstrap
                 'twig.path' => APPLICATION_PATH . '/../views',
             )
         );
+
+        //reports
+        $reports = array(
+            array(
+                'id' => 1,
+                'name' => 'Report 1',
+                'description' =>"Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain's going to be? He's the exact opposite of the hero."
+                ),
+            array(
+                'id' => 2,
+                'name' => 'Report Other',
+                'description' => "And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass."
+                )
+            );
+
         // definitions
         $app->get(
             '/',
-            function () use ($app) {
-                return $app['twig']->render('index.html');
+            function () use ($app, $reports) {
+                return $app['twig']->render('index.twig', array('reports'=>$reports));
+            }
+        );
+
+        $app->get(
+            '/report/show/{id}',
+            function (Silex\Application $app, $id) use ($reports) {
+                switch ($id) {
+                    case 1:
+                        $data = array(
+                            'headers'=>array(
+                                'Name', 'Age'
+                            ), 
+                            'rows'=> array(
+                                array('John', 12), 
+                                array('Eric', 28)
+                                )
+                            );
+                        break;
+                    default:
+                        $app->abort(404, "Post $id does not exist.");
+                }
+
+                return $app['twig']->render('report.twig', array('report'=>$reports[$id-1], 'data'=>$data));
+
+
+
+
             }
         );
 
---
+++ application/tests/unit/Bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../../../application/'));
+defined('VENDOR_PATH') || define('VENDOR_PATH', realpath(dirname(__FILE__) . '/../../../vendor/'));
+set_include_path(implode(PATH_SEPARATOR, array(
+  APPLICATION_PATH, VENDOR_PATH, get_include_path()
+)));
+require_once 'autoload.php';

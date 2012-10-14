6a2dc43ee9f14ec99f675fe6df3855b8075ae29a
Jason <jasonrobertfox@gmail.com>
2012-10-13 21:23:49 -0400

    updated bootstrap to run the basic view and adjusted configurations

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
-defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(dirname(_FILE_) . '/../application/'));
-defined('VENDOR_PATH') || define('VENDOR_PATH', realpath(dirname(_FILE_) . '/../vendor/'));
+defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application/'));
+defined('VENDOR_PATH') || define('VENDOR_PATH', realpath(dirname(__FILE__) . '/../vendor/'));
 set_include_path(implode(PATH_SEPARATOR, array(
   APPLICATION_PATH, VENDOR_PATH, get_include_path()
 )));
